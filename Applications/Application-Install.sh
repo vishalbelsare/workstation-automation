00046f6c84ae9dd33c124e2305c7b232e2383f08
Brian Flanagan <bflanagan@quantopian.com>
2018-04-09 16:34:58 -0400

    Structural Changes
    Added tmp dir logic
    disk counter, unmount logic
    Trap

--- Applications/Application-Install.sh
+++ Applications/Application-Install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #Working Folder Paths
 desktop="${HOME}/Desktop/"
-tmppath="${HOME}/Desktop/tmp/"
 apppath="/Applications"
+tmppath="$(mktemp -d /tmp/qinstall_XXX)"
+counter=1
 
 #Create temp directory to download .dmg files to
-mkdir $tmppath
-tar -xzf EraAgentInstallerMac.tar.gz -C $tmppath
-cp -r ApplicationDownloads.csv $tmppath
+cp -r -p ApplicationDownloads.csv $tmppath
 cd $tmppath
 
 #Download DMG files from respective sources
@@ -15,7 +14,8 @@ awk -F"\"*,\"*" '{cmd="curl -s -Lo"$1 " " $2; system(cmd)}' ApplicationDownloads
 
 #Mounts all DMG Files
 for f in *.dmg ;
-	do hdiutil mount "$f" -nobrowse 
+	do hdiutil mount "$f" -nobrowse
+	let "counter++" 
 done
 
 #Copies .App files directly into Applications folder, and .pkg files to tmp folder for processing [---Want to refactor this with a loop]
@@ -38,32 +38,22 @@ cd $tmppath
 for f in *.pkg ;
 	do sudo installer -verbose -pkg "$f" -target /
 done
-#Installs ESET Antivirus
-bash EraAgentInstaller.sh
-#Copies LastPass folder to Desktop and runs installer
-cd $tmppath
-cp -r LastPass\ Installer $desktop
-cd $desktop
+
 #Open Chrome so that the LastPass installer recognizes it as a browser on the system
 open /Applications/Google\ Chrome.app
 sleep 5
 open LastPass\ Installer/LastPass\ Installer.app 
-
 echo "All applications have been installed and moved to the /Applications directory"
-echo "Opening Airwatch to continue the enrollment process"
-open /Applications/VMware\ Airwatch\ Agent.app
 
-#Ejects all mounted volumes
-for num in {2..10}
+#Ejects all mounted volumes besides for Macintosh HD (/devdisk0 and /devdisk1)
+for num in $(seq 2 $counter)
 do
 	diskutil eject /dev/disk$num
 done
 echo "All Installer DMGs have been ejected"
 
-#Deletes /tmp
-rm -rf $tmppath
-
-
-
-
-
+#Deletes temp directory
+function finish {
+rm -rf "$tmppath"
+}
+trap finish EXIT
