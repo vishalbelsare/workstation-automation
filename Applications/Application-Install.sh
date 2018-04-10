741eccc4818eb5c1d708dd20cf5055cad0466528
Brian Flanagan <bflanagan@quantopian.com>
2018-04-10 12:38:45 -0400

    Refactoring

--- Applications/Application-Install.sh
+++ Applications/Application-Install.sh
@@ -8,8 +8,7 @@ counter=1
 #Copy download list to temp file, cd into temp dir
 cp -r -p ApplicationDownloads.csv $tmppath && cd $tmppath
 
-#Download DMG files from respective sources specified in .csv file
-#Column 1 is the name of the file, Column 2 is the download source
+#Download DMG files from respective sources specified in .csv file ($1=name 2=source)
 awk -F"\"*,\"*" '{cmd="curl -s -Lo"$1 " " $2; system(cmd)}' ApplicationDownloads.csv 
 
 #Mounts all DMG Files, and increments a counter to be used in eject sequence
@@ -18,9 +17,9 @@ for f in *.dmg ;
 	let "counter++" 
 done
 
-#Copies all .app files to /Applications folder and all .pkg files to /tmp for processing
-sudo rsync -av /Volumes/*/*.app $apppath --exclude /Volumes/Macintosh\ HD
+#Copies all .pkg and .zip files to tmppath for processing
 sudo rsync -av /Volumes/*/*.pkg $tmppath --exclude /Volumes/Macintosh\ HD
+sudo rsync -av /Volumes/*/*.zip $tmppath --exclude /Volumes/Macintosh\ HD
 
 #Unzips all zip files in /tmp
 for f in *.zip ;
@@ -28,17 +27,18 @@ for f in *.zip ;
 done
 
 #Runs each of the installers in /tmp
-cd $tmppath
 for f in *.pkg ;
 	do sudo installer -verbose -pkg "$f" -target /
 done
 
-#Open Chrome so that the LastPass installer recognizes it as a browser on the system
-open /Applications/Google\ Chrome.app
-sleep 5
-open LastPass\ Installer/LastPass\ Installer.app 
-echo "All applications have been installed and moved to the /Applications directory"
+#Copies all .app files to /Applications folder from /Volumes and tmppath
+sudo rsync -av /Volumes/*/*.app $apppath --exclude /Volumes/Macintosh\ HD
+sudo rsync -av $tmp/*.app $apppath --exclude /Volumes/Macintosh\ HD
+rsync -av $tmp/LastPass\ Installer/*.app $apppath
 
+#Open Chrome so that the LastPass installer recognizes it as a browser on the system
+open /Applications/Google\ Chrome.app && sleep 5 && open /Applications/LastPass\ Installer.app
+echo "All applications have been installed and moved to the /Applications directory"
 
 #Ejects all mounted volumes besides for Macintosh HD (/devdisk0 and /devdisk1)
 for num in $(seq 2 $counter)
